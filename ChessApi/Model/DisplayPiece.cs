9a5d050e00237ba7107b3d167eefeb02c5184921
Xandrf <oliveira_carlos2017@outlook.com.br>
2021-08-22 06:31:35 -0300

    merge resolved

---
+++ ChessApi/Model/DisplayPiece.cs
@@ -0,0 +1,23 @@
+using System;
+using System.IO;
+using ChessCore;
+namespace ChessApi.Model
+{
+    public class DisplayPiece
+    {
+        public int id { get; set; }
+        public DisplayPosition dPosition {get;set;}
+        public bool isAlive {get;set;}
+        public string color {get;set;}
+        public DisplayPiece(DisplayPosition dpos,string color){
+            
+            this.dPosition=new DisplayPosition(dpos.x,dpos.y);
+            this.color = color;
+            isAlive = true;
+        }
+          public DisplayPiece(char x,int y,string color){
+            this.dPosition=new DisplayPosition(x,y);
+            this.color = color;
+        }
+    }
+}
